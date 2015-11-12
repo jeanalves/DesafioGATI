dd60499a8ea794be2a11884e40151f3bb41076df
Jean carlos <jeanc.alves@hotmail.com>
2015-11-12 07:54:37 -0200

    Criado a função Atualizar e melhorado a forma de objeto com o metodo retornaCliente()

--- DesafioGATI/src/desafiogati/CRUD.java
+++ DesafioGATI/src/desafiogati/CRUD.java
@@ -1,13 +1,8 @@
 package desafiogati;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class CRUD {
@@ -18,7 +13,7 @@ public class CRUD {
         
         while(true){
             
-            System.out.println("\n----------------CRUD----------------\n");
+            System.out.println("\n\n----------------CRUD----------------\n");
             System.out.println("1 - Cadastrar");
             System.out.println("2 - Listagem");
             System.out.println("3 - Atualizar");
@@ -33,7 +28,7 @@ public class CRUD {
                 listagem();
             }
             else if(opcao == 3){
-
+                atualizar();
             }
             else if(opcao == 4){
                 deletar();
@@ -44,20 +39,9 @@ public class CRUD {
         }
     }
     
-    /*
-    private String nome;
-    private String email;
-    private String telefone;
-    private String Endereco;
-    private String numero;
-    private String bairro;
-    private String cep;
-    private boolean ativo;
-    */
-    
     public void cadastrar(){
         
-        System.out.println("\n----------------Cadastro----------------\n");
+        System.out.println("\n\n----------------Cadastro----------------\n");
         Cliente cliente = new Cliente();
         
         System.out.print("Digite um id : "); cliente.setId(readInt());
@@ -68,16 +52,17 @@ public class CRUD {
         System.out.print("Digite um numero : "); cliente.setNumero(readString());
         System.out.print("Digite um bairro : "); cliente.setBairro(readString());
         System.out.print("Digite um cep : "); cliente.setCep(readString());
-        System.out.print("Digite um Ativo (S/N) : "); cliente.setAtivo(readYN());
+        System.out.print("Ativo? (S/N) : "); cliente.setAtivo(readYN());
         
+        //Adciona o objeto cliente a ArrayList clientes
         this.clientes.add(cliente);
         
         System.out.println("Cliente cadastrado com sucesso");
-        
+        pausa();
     }
     
     public void listagem(){
-        System.out.println("\n----------------Listagem----------------\n");
+        System.out.println("\n\n----------------Listagem----------------\n");
         for(Cliente cli: this.clientes){
             System.out.println(
                                 cli.getId() + " " +
@@ -91,57 +76,74 @@ public class CRUD {
                                 cli.isAtivo()
                     );
         }
+        pausa();
+    }
+    
+    public void atualizar(){
+        System.out.println("\n\n----------------Atualizar----------------\n");
         
+        Cliente cliente = new Cliente();
+        
+        System.out.print("Digite o ID do cliente que deseja altarar : "); cliente.setId(readInt());
+        
+        //Busca o objeto cliente e se o mesmo existir o altera
+        cliente = retornaCliente(cliente.getId());
+        
+        if(cliente != null){
+            
+            System.out.print("Digite um novo nome : "); cliente.setNome(readString());
+            System.out.print("Digite um novo email : "); cliente.setEmail(readString());
+            System.out.print("Digite um novo telefone : "); cliente.setTelefone(readString());
+            System.out.print("Digite um novo endereco : "); cliente.setEndereco(readString());
+            System.out.print("Digite um novo numero : "); cliente.setNumero(readString());
+            System.out.print("Digite um novo bairro : "); cliente.setBairro(readString());
+            System.out.print("Digite um novo cep : "); cliente.setCep(readString());
+            System.out.print("Ativo? (S/N) : "); cliente.setAtivo(readYN());
+
+            System.out.println("\nCliente atualizado com sucesso!");
+            
+        }
+        else{
+            System.out.println("\nO ID informado não existe, tente novamente!");
+        }
+        pausa();
     }
     
     public void deletar(){
-        System.out.println("\n----------------Deletar----------------\n");
+        System.out.println("\n\n----------------Deletar----------------\n");
         System.out.print("Digite o id do cliente que deseja deletar : ");
         int id = readInt();
         
-        Cliente cliDel = null;
-        
-        //Procura pelo objeto que tem o id informado e o remove
-        for(Cliente cli: this.clientes){
-            
-            if(cli.getId() == id){
-                cliDel = cli;
-            }
-            
-        }
-        
-        this.clientes.remove(cliDel);
+        this.clientes.remove(retornaCliente(id));
         
         System.out.println("Cliente removido com sucesso!");
-        
+        pausa();
     }
+    
+    
+    
     //Funçoes do CRUD
     
     //Faz a leitura do teclado e retorna uma string
-    public static String readdString(){
-        String read;
-        try (Scanner scanIn = new Scanner(System.in)) {
-            read = scanIn.nextLine();
-        }
-        
-        
-        return read;
-    }
-    
     public static String readString(){
         String read = "";
-        try {
-            BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-            read = bufferRead.readLine();
-        } catch (IOException ex) {
-            Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        try{
+            Scanner scanIn = new Scanner(System.in);
+            read = scanIn.nextLine();
+        }catch(Exception e){
+            
+        }        
         return read;
-    }    
+    }
+        
     //Faz a leitura do teclado e retorna um valor inteiro
     public int readInt(){
-        Scanner scanIn = new Scanner(System.in);
-        int read = Integer.parseInt(scanIn.nextLine());
+        int read = 0;
+        try{
+            Scanner scanIn = new Scanner(System.in);
+            read = Integer.parseInt(scanIn.nextLine());
+        }catch(Exception e){   
+        }
         return read;
     }
     
@@ -170,6 +172,29 @@ public class CRUD {
         }while(read != "s"|| read != "n");
         
         return false;
+    }
+    
+    //Verifica se existe cliente e retorna objeto se o achou
+    public Cliente retornaCliente(int id){
+        
+        for(Cliente  cli : this.clientes){
+            
+            if(cli.getId() == id){
+                return cli;
+            }
+            
+        }
+        return null;
+    }
+    
+    //Pausa a linha de texto para melhor visualizala
+    public void pausa(){
+        System.out.print("\nTecle qualquer tecla para continuar!");
+        try{
+            Scanner scanIn = new Scanner(System.in);
+            String read = scanIn.nextLine();
+        }catch(Exception e){
+        }
     }
     
 }
